e3a4d18a29a21100879aeecca1e3364de9e64c17
Artem Shnurko <shnurko.artyom@gmail.com>
2022-08-02 00:17:35 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const IngredientsEl = ingredients.map((ingredient) => {
+  const ingredientItems = document.createElement("li");
+  ingredientItems.classList.add("item");
+  ingredientItems.textContent = ingredient;
+  return ingredientItems;
+});
+
+const listIngredients = document.querySelector("#ingredients");
+listIngredients.textContent = "For soup you need:";
+listIngredients.append(...IngredientsEl);
