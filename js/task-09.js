e052a3ebffd5c505765a1e97cafc29dfcaade0ac
Artem Shnurko <shnurko.artyom@gmail.com>
2022-08-05 15:59:06 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bg = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", onChangeBg);
+
+function onChangeBg() {
+  bg.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
